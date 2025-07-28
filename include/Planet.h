9b9f52f2a6eaae565fa3d09bd9324075931001c4
aditiingle <aditiingle14@gmail.com>
2025-07-28 18:49:01 -0400

    Make Moon class parametrized for customizable Moons and add Mars

--- include/Planet.h
+++ include/Planet.h
@@ -3,72 +3,83 @@
 #include "CelestialBody.h"
 
 // Specific implementation for planets
-class Planet : public CelestialBody {
+class Planet : public CelestialBody
+{
 public:
-    Planet(const std::string& texturePath, float planetScale = 1.0f);
-    
+    Planet(const std::string &texturePath, float planetScale = 1.0f);
+
 protected:
     std::string getTexturePath() override { return texturePath_; }
-    
+
 private:
     std::string texturePath_;
 };
 
 // Earth with specific properties
-class Earth : public Planet {
+class Earth : public Planet
+{
 public:
     Earth();
 };
 
-class Moon : public Planet {
+class Moon : public Planet
+{
 public:
-    Moon(CelestialBody* parent);
+    Moon(CelestialBody *parent, float orbitalRadius = 10.0f, float orbitalSpeed = 1.5f, float rotationSpeed = 1.0f);
 };
 
-// Mars with specific properties  
-class Mars : public Planet {
+// Mars with specific properties
+class Mars : public Planet
+{
 public:
     Mars();
 };
 
 // Venus with specific properties
-class Venus : public Planet {
+class Venus : public Planet
+{
 public:
     Venus();
 };
 
 // Jupiter with specific properties
-class Jupiter : public Planet {
+class Jupiter : public Planet
+{
 public:
     Jupiter();
 };
 
 // Saturn with specific properties
-class Saturn : public Planet {
+class Saturn : public Planet
+{
 public:
     Saturn();
 };
 
 // Mercury with specific properties
-class Mercury : public Planet {
+class Mercury : public Planet
+{
 public:
     Mercury();
 };
 
 // Uranus with specific properties
-class Uranus : public Planet {
+class Uranus : public Planet
+{
 public:
     Uranus();
 };
 
 // Neptune with specific properties
-class Neptune : public Planet {
+class Neptune : public Planet
+{
 public:
     Neptune();
 };
 
 // Pluto with specific properties
-class Pluto : public Planet {
+class Pluto : public Planet
+{
 public:
     Pluto();
 };
